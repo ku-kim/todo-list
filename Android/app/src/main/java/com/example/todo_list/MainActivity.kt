c97eea3df2e45bd9e5e8b3c8ce583a6f1fc920da
DESKTOP-UIQTHKE\User <shn0185@naver.com>
2022-04-07 16:08:22 +0900

    feat: Ui,Data 역할분리

--- Android/app/src/main/java/com/example/todo_list/MainActivity.kt
+++ Android/app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.todo_list.databinding.ActivityMainBinding
+import com.example.todo_list.history.data.HistoryRepository
 import com.example.todo_list.model.HistoryCard
 import com.example.todo_list.model.HistoryReceive
 import com.example.todo_list.model.Todos
 import com.google.android.material.navigation.NavigationView
 import com.google.gson.JsonArray
+import kotlinx.coroutines.CoroutineScope
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,45 +32,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawerLayout = findViewById(R.id.main_layout)
 
-        binding.btnMenu.setOnClickListener { drawerLayout.openDrawer(GravityCompat.END) }
+        binding.btnMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.END)
+        }
 
         navigationView = findViewById(R.id.navi_view)
         navigationView.setNavigationItemSelectedListener(this)
 
-        binding.btnNetwork2?.setOnClickListener {
-            HistoryReceive.service.getHistory("histories")?.enqueue(object : Callback<List<HistoryCard>> {
-
-                override fun onResponse(call: Call<List<HistoryCard>>, response: Response<List<HistoryCard>>) {
-                    Log.d("onResponse", "${response.body()}")
-                    Log.d("onResponse", response.message())
-
-                }
-
-                override fun onFailure(call: Call<List<HistoryCard>>, t: Throwable) {
-                    Log.d("onFailure", "통신 실패")
-                    Log.d("onFailure", "${t.message}")
-                    //<Todos>는 Array(배열)지만 예상한것은 Object(객체)였음
-                    // <JsonArray>로 하면 받아지는데 왜? >> <JsonArray>는 객체를 받아서 배열로바꿔줌?.
-                }
-            })
-        }
-
-        binding.btnNetwork?.setOnClickListener {
-            HistoryReceive.service.getTodos("todos")?.enqueue(object : Callback<List<Todos>> {
-
-                override fun onResponse(call: Call<List<Todos>>, response: Response<List<Todos>>) {
-                    Log.d("onResponse", "${response.body()}")
-                    Log.d("onResponse", response.message())
-                }
-
-                override fun onFailure(call: Call<List<Todos>>, t: Throwable) {
-                    Log.d("onFailure", "통신 실패")
-                    Log.d("onFailure", "${t.message}")
-                    Log.d("onFailure", "${call}")
-                    //<Todos>는 Array(배열)지만 예상한것은 Object(객체)였음
-                    // <JsonArray>로 하면 받아지는데 왜? >> <JsonArray>는 객체를 받아서 배열로바꿔줌?.
-                }
-            })
+        val str = HistoryRepository().getHistory()
+        str.observe(this) {
+            Log.d("LIVE", it.toString())
         }
     }
 
--- Android/app/src/main/java/com/example/todo_list/model/HistoryReceive.kt
+++ Android/app/src/main/java/com/example/todo_list/model/HistoryReceive.kt
@@ -1,7 +1,7 @@
 package com.example.todo_list.model
 
-import com.google.gson.JsonArray
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -14,7 +14,7 @@ class HistoryReceive {
                 .baseUrl("https://f278a12c-c825-466b-aa01-65337bbdf28a.mock.pstmn.io/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
-        val service = retrofit.create(HistoryApi::class.java)
+        val service: HistoryApi = retrofit.create(HistoryApi::class.java)
     }
 }
 
