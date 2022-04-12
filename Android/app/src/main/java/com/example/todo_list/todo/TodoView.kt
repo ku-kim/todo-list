db43c3aafda1fb1cfee24c34de2c0eb552829671
banjjak2 <interr0bang@naver.com>
2022-04-12 11:51:28 +0900

    feat: Todo 커스텀 뷰 구현
    동일한 뷰가 중복되어 사용된다고 판단해 커스텀 뷰로 구현 진행

---
+++ Android/app/src/main/java/com/example/todo_list/todo/TodoView.kt
@@ -0,0 +1,50 @@
+package com.example.todo_list.todo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.todo_list.R
+
+class TodoView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvBadgeCount: TextView
+    private lateinit var btnAddTask: ImageButton
+    init {
+        initViews()
+        initAttributes(attrs)
+    }
+
+    private fun initAttributes(attrs: AttributeSet?) {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.TodoView,
+            0,
+            0).apply {
+                try {
+                    tvTitle.text = getString(R.styleable.TodoView_title)
+                    tvBadgeCount.text = getString(R.styleable.TodoView_badge_count)
+                } finally {
+                    recycle()
+                }
+        }
+    }
+
+    private fun initViews() {
+        val layoutInflater = context.getSystemService(
+            Context.LAYOUT_INFLATER_SERVICE
+        ) as LayoutInflater
+
+        addView(
+            layoutInflater.inflate(R.layout.todo_view, this, false)
+        )
+
+        tvTitle = findViewById(R.id.todo_title)
+        tvBadgeCount = findViewById(R.id.todo_badge)
+        btnAddTask = findViewById(R.id.btn_task_add)
+    }
+}
